220b8fda5c7b28d83fe74b15537875e6580fbb3a
PrzeChoj <premysl.choj@gmail.com>
2023-04-29 17:01:17 +0200

    Save implementation

--- 3SATSolver/3SATSolverLib/FormulaReader.cs
+++ 3SATSolver/3SATSolverLib/FormulaReader.cs
@@ -80,12 +80,47 @@ namespace _3SATSolverLib
     {
         public static void WriteToFile(string filename, Formula formula)
         {
-            throw new NotImplementedException();
+            string? toPrint = GetSolution(formula);
+
+            if (toPrint == null)
+                return;
+            
+            File.WriteAllText(filename, toPrint);
         }
 
         public static void WriteToConsole(Formula formula)
         {
-            throw new NotImplementedException();
+            string? toPrint = GetSolution(formula);
+
+            if (toPrint == null)
+                return;
+            
+            Console.WriteLine(toPrint);
+        }
+
+        private static string? GetSolution(Formula formula)
+        {
+            if (!formula.IsSolved)
+            {
+                Console.WriteLine("Cannot save the unsolved formula!");
+                return null;
+            }
+
+            if (formula.Solution == null)
+            {
+                return "NO";
+            }
+
+            string[] solutions = new string[formula.MaxVariableIndex];
+
+            for (int i = 0; i < formula.MaxVariableIndex; i++)
+            {
+                solutions[i] = i.ToString() + "=" + formula.Solution[i];
+            }
+
+            string solution = string.Join(", ", solutions);
+
+            return string.Join("\n", "YES", solution);
         }
     }
 }
