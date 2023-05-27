e011cf6f47ee7c62aec6d95bf95362bec6e21c83
PrzeChoj <premysl.choj@gmail.com>
2023-05-27 19:26:00 +0200

    Wykres analiza czasowa

--- analizaWynikow/analizaWynikow.R
+++ analizaWynikow/analizaWynikow.R
@@ -12,22 +12,42 @@ summary(my_tibble)
 
 lm(log10(timeMs) - log10(variables) ~ variables, data = my_tibble)
 
+a_log10 <- -0.350414
+c_log10 <- 0.004781
+
 time_estimate <- function(n, a_log10 = -0.350414, c_log10 = 0.004781){
   (10^a_log10)*n*((10^c_log10)^n)
 }
 
-a_log10 <- -0.350414
-c_log10 <- 0.004781
-time_estimate(c(100, 200), a_log10, c_log10)
+time_estimate(c(100, 200))
 
 
-
-my_tibble %>% 
+g <- my_tibble %>% 
   dplyr::mutate(estimated_time = time_estimate(variables)) %>% 
   ggplot2::ggplot(ggplot2::aes(variables, timeMs)) +
-  ggplot2::geom_jitter(width = 1) +
+  ggplot2::geom_jitter(ggplot2::aes(color = "Data Points"), width = 1) +
   ggplot2::scale_y_log10() +
   ggplot2::scale_x_continuous(breaks = unique(my_tibble$variables)) +
-  ggplot2::geom_line(ggplot2::aes(variables, estimated_time, colour = "red"))
-  
+  ggplot2::geom_function(ggplot2::aes(color = "Time Estimate"), fun = time_estimate) +
+  ggplot2::ggtitle("Czas potrzebny na rozwiązanie problemu 3SAT") +
+  ggplot2::xlab("Liczba zmiennych w formule") + ggplot2::ylab("Czas (w milisekundach)") +
+  ggplot2::labs(colour = "", subtitle = "pominięto obserwacje dla ktorych czas = 0ms") +
+  ggplot2::scale_color_manual(
+    labels = c("Obserwacje",
+               expression(0.45 * italic("n") * (1.011)^n)),
+    values = c("black", "red")
+  ) +
+  ggplot2::theme(
+    plot.title = ggplot2::element_text(size=14, face="bold"),
+    axis.title.x = ggplot2::element_text(size=14, face="bold"),
+    axis.title.y = ggplot2::element_text(size=14, face="bold"),
+    panel.grid.minor.x = ggplot2::element_blank()
+  )
+
+g
+
+ggplot2::ggsave("wykres_czasu.png", g, width = 11, height = 5)
+
+
+
 
